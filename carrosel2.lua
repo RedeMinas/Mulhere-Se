32215a10bc25af616d92b810090ea8f1f38eb511
BernardoM <bernardo.neto@sga.pucminas.br>
2016-02-18 15:11:24 -0200

    i changed and i readjusted the photos  region

---
+++ carrosel2.lua
@@ -0,0 +1,89 @@
+
+function findImages()
+
+	os.execute("find Media/images/ -name *.jpg > imagelist.txt")
+	os.execute("find Media/images/ -name *.JPG >> imagelist.txt")
+	os.execute("find Media/images/ -name *.jpeg >> imagelist.txt")
+	os.execute("find Media/images/ -name *.bmp >> imagelist.txt")
+	os.execute("find Media/images/ -name *.gif >> imagelist.txt")
+	os.execute("find Media/images/ -name *.png >> imagelist.txt")
+
+    local imagesMenu = {}
+
+    for line in io.lines("imagelist.txt") do
+      print(#imagesMenu .. " - ".. line)
+      table.insert(imagesMenu, line)
+    end
+
+    return imagesMenu
+end
+
+function showImage(images, index)
+  if #images > 0 then
+    canvas:drawRect('fill', 0, 0, canvas:attrSize());
+    img = canvas:new(images[index])
+    canvas:compose(10, 10, img)
+    registerTimer()
+    canvas:flush()
+  end
+end
+
+function moveImageIndex(images, index, forward)
+  if forward then
+     index = index + 1
+     if index > #images then
+        index = 1
+     end
+  else
+     index = index - 1
+     if index <= 0 then
+        index = #images
+     end;
+  end
+  return index
+end
+
+local index = 1
+local images = findImages()
+
+function autoForward()
+  index = moveImageIndex(images, index, true)
+  showImage(images, index)
+end
+
+function registerTimer()
+
+  local timeout = 2000
+
+
+  if cancelTimerFunc then
+     cancelTimerFunc()
+  end
+  cancelTimerFunc = event.timer(timeout, autoForward)
+end
+
+function handler(evt)
+
+    showImage(images, index)
+    print("Show Image: "..index)
+
+    print("Evento disparado: " .. evt.class .. " " .. evt.type)
+    if (evt.class == 'key' and evt.type == 'press') then
+
+      if evt.key == "CURSOR_UP" then
+        index = moveImageIndex(images, index, true)
+      elseif evt.key == "CURSOR_DOWN" then
+         index = moveImageIndex(images, index, false)
+      elseif evt.key == "EXIT" then
+        print ('chegou!')
+
+      end
+
+    elseif evt.class == "ncl" and evt.type=="presentation" and evt.action=="start" then
+      autoForward()
+
+    end
+
+end
+
+event.register(handler)
